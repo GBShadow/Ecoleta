f4624471470c45ae3ef20228b4fb480de06622ea
Shadow <gbshadow@pop-os.localdomain>
2020-08-28 17:15:06 -0300

    fix

--- ecoleta/App.tsx
+++ ecoleta/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { AppLoading } from 'expo';
-import { StatusBar } from 'react-native';
-import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
-import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import React from 'react'
+import { AppLoading } from 'expo'
+import { StatusBar } from 'react-native'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
-import Routes from './src/routes';
+import Routes from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
     Ubuntu_700Bold
-  });
+  })
 
   if (!fontsLoaded) {
     return <AppLoading />
@@ -19,8 +19,12 @@ export default function App() {
 
   return (
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Routes />
     </>
-  );
+  )
 }
